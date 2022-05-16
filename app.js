ec076f6522ba1f3f702862d82df140ed09a015a8
Pavandeep Singh <pavandeepsingh1012@gmail.com>
2022-05-16 13:18:03 +0530

    starting project

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// pug
-// app.set('view engine', 'pug');
-// app.set('views','views')
-
-//Handle
-// const expressHbs = require('express-handlebars');
-// app.engine('hbs', expressHbs({layoutsDir: "views/layouts/", defaultLayout: "main-layout", extname: 'hbs' }));
-// app.set('view engine', 'hbs');
-// app.set('views','views')
-
-
-// EJS
 app.set('view engine', 'ejs');
-app.set('views','views')
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -26,9 +16,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const errorController = require('./controllers/error')
-
-app.use("/admin",adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- controllers/error.js
+++ controllers/error.js
@@ -1,13 +1,3 @@
 exports.get404 = (req, res, next) => {
-    // res.status(404).send(`<div style="width:100%; display:flex; justify-content:center;">
-    //             <h1>Page not found</h1>
-    //             </div>`);
-
-    // res.sendFile(path.join(__dirname,"views","404.html"));
-
-    // res.status(404).render("404.pug",{pageTitle:"Page Not Found"})
-
-    // res.status(404).render("404.hbs",{pageTitle:"Page Not Found"})
-
-    res.status(404).render("404.ejs",{pageTitle:"Page Not Found"})
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
